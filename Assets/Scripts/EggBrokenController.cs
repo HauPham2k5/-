2b6d05490a53041595b6ca7afa3237853bab4465
HauPham2k5 <hauphpd10095@gmail.com>
2024-03-18 12:15:38 +0700

    first commit

---
+++ Assets/Scripts/EggBrokenController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EggBrokenController : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    void Start()
+    {
+        Destroy(gameObject, 0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
